7f871a9043fe5a0749936caedad2ad863bbfa254
Tom Rowson <tom.rowson@gmail.com>
2018-10-22 17:54:04 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,10 +2,11 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name: 
+        name: "Lord Montague",
+        age: 53
         },
       :matriarch => {
-        
+        name
         },
       :hero => {
         
