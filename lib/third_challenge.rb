95a9c240879e3ac09cd004b642f14a07aaa57f8a
Tom Rowson <tom.rowson@gmail.com>
2018-10-22 17:53:46 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,13 +2,13 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        name: 
         },
       :matriarch => {
-        #your key/value pairs here
+        
         },
       :hero => {
-        #your key/value pairs here
+        
         },
       :hero_friends => []
    }, 
