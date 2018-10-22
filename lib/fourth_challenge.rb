700f0e0f222dc93c648af886781335f66bb27cd9
Tom Rowson <tom.rowson@gmail.com>
2018-10-22 18:16:02 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,9 +9,8 @@ def fourth_challenge
         age: "17",
         attitude: "worried"},
         {name: "Mercutio",
-        age: 
-          
-        }
+        age: "18",
+        attitude: "hot-headed"}
         }
       ]
    }, 
@@ -20,7 +19,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {name: "Steven", 
+        age: "30",
+        attitude: "confused"},
+        {name: "Nurse",
+        age: "44",
+        attitude: "worried"}
       ]
    }
 }
